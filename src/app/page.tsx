b93f157de7bd348bd8aa7d6bccce6e1c40dd7490
rrhmn <raissarahmani18@gmail.com>
2025-09-09 11:02:47 +0700

    fix: authentication route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, SignedOut, SignedIn } from "@clerk/nextjs";
 import Header from "./components/header";
+import Home from "./home/page";
 
 export default function HomePage() {
   const {isSignedIn} = useAuth();
@@ -27,19 +28,25 @@ export default function HomePage() {
      }}
      className="relative min-h-screen"
     >
-      <div><Header /></div>
-      <div className="absolute inset-0 flex flex-col justify-center items-center px-30">
-        <Image
-          src="/logo2.png"
-          alt="Molca Logo"
-          width={70}
-          height={70}
-          className="object-contain"
-        />
-        <p className="text-5xl/15 text-center font-semibold title py-5 px-60">Operating System for Industrial Infrastructure</p>
-        <p className="text-sm text-center px-100">Powered by Digital Twin, AR/VR, & Smart Manufacturing Solutions</p>
-        <button type="button" onClick={handleLogin} className="button bg-[var(--color-primary)] text-[var(--color-base)] w-1/2">Login</button>
-      </div>
+      <SignedOut>
+        <div><Header /></div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center px-30">
+          <Image
+            src="/logo2.png"
+            alt="Molca Logo"
+            width={70}
+            height={70}
+            className="object-contain"
+          />
+          <p className="text-5xl/15 text-center font-semibold title py-5 px-60">Operating System for Industrial Infrastructure</p>
+          <p className="text-sm text-center px-100">Powered by Digital Twin, AR/VR, & Smart Manufacturing Solutions</p>
+          <button type="button" onClick={handleLogin} className="button bg-[var(--color-primary)] text-[var(--color-base)] w-1/2">Login</button>
+        </div>
+      </SignedOut>
+
+      <SignedIn>
+        <Home />
+      </SignedIn>
     </main>
   );
 }
