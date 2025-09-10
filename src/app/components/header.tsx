7c9213543a001fa4dc8c0270f2a4a3771f230840
rrhmn <raissarahmani18@gmail.com>
2025-09-10 08:59:55 +0700

    fix: styling

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,16 @@
 "use client"
 
 import Image from "next/image"
+import Link from "next/link"
 import { useState } from "react"
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+import { SignedIn, UserButton } from "@clerk/nextjs"
 
 export default function Header() {
   const [active, setActive] = useState('')
   const options = ["Digital Twin", "VR", "AR", "Smart Manufacturing", "Deck", "Tools"]
 
   return (
-    <div className="w-full py-1 px-5 flex flex-row justify-between items-center">
+    <div className="w-full py-1 px-5 flex flex-row justify-between items-center relative">
       <Image
         src="/logo.png"
         alt="Molca Logo"
@@ -28,21 +29,27 @@ export default function Header() {
                 active === option
                   ? "text-[var(--color-primary)] font-semibold border-b-2 border-[var(--color-primary)]"
                   : "text-[var(--color-text)]"
-              } text-sm hover:text-[var(--color-primary)] hover:font-semibold hover:cursor-pointer`}
+              } text-sm hover:text-[var(--color-primary)] hover:scale-105 hover:duration-200 hover:cursor-pointer`}
             >
               {option}
             </button>
           ))}
         </div>
         
-        <div>
+        <div className="flex flex-row gap-3 items-center px-3">
+          <Link href="/dashboard" className="flex flex-row gap-2 button m-0 border bg-[var(--color-primary)] text-[var(--color-base))]">
+            <Image
+              src="/go.svg"
+              alt="Go to dashboard"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <p className="font-semibold">Dashboard</p>
+          </Link>
           <UserButton />
         </div>
       </SignedIn>
-
-      <SignedOut>
-
-      </SignedOut>
     </div>
   )
 }
--- src/app/components/pagination.tsx
+++ src/app/components/pagination.tsx
@@ -11,7 +11,7 @@ export default function pagination({ page, active, onClick }: PaginationProps) {
   return (
     <div 
       onClick={onClick} 
-      className={`${active === true ? "bg-[var(--color-primary)] text-[var(--color-base)]" : "bg-[var(--color-grey)] text-[var(--color-text)]" } rounded-md flex flex-col items-center justify-center w-[25px] h-[25px] text-xs cursor-pointer hover:bg-[var(--color-primary)] hover:text-[var(--color-base)]`}
+      className={`${active === true ? "bg-inherit text-inherit" : "bg-[var(--color-grey)] text-[var(--color-text)]" } rounded-md flex flex-col items-center justify-center w-[25px] h-[25px] text-xs cursor-pointer hover:bg-inherit hover:text-inherit`}
     >
       {page}
     </div>
