ca1154392f81f1b08440cb66aeb652b2b46bb98f
rrhmn <raissarahmani18@gmail.com>
2025-09-12 14:29:26 +0700

    fix: project link passed through state

--- src/app/components/home/card.tsx
+++ src/app/components/home/card.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image"
+import Link from "next/link"
 
 type CardProps = {
   image: string;
   title: string;
+  link: string;
 }
 
-export default function card({ title }: CardProps) {
+export default function card({ title, link }: CardProps) {
   return (
-    <div className='flex flex-col items-center justify-center bg-[var(--color-text)] rounded-lg relative max-w-[250px] h-[300px] cursor-pointer hover:scale-105 hover:duration-200'>
+    <Link 
+      href={link}
+      className='flex flex-col items-center justify-center bg-[var(--color-text)] rounded-lg relative max-w-[250px] h-[300px] cursor-pointer hover:scale-105 hover:duration-200'
+    >
       <Image
         src="/noimage.png"
         alt="No image"
@@ -18,6 +23,6 @@ export default function card({ title }: CardProps) {
       <div className="z-10 absolute bottom-0 inset-x-0 bg-[#00000099] px-5 py-3">
         <div className="font-semibold text-sm text-[var(--color-primary)]">{title}</div>
       </div>
-    </div>
+    </Link>
   )
 }
