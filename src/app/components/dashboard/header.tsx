b7fd3c2abda360d64045f18e4066ef97b0d97eb2
rrhmn <raissarahmani18@gmail.com>
2025-09-12 14:33:49 +0700

    fix: nav added

--- src/app/components/dashboard/header.tsx
+++ src/app/components/dashboard/header.tsx
@@ -6,8 +6,8 @@ import { useState } from "react"
 import { SignedIn, UserButton } from "@clerk/nextjs"
 
 export default function Header() {
-  const [active, setActive] = useState('')
-  const options = ["Digital Twin", "VR", "AR", "Smart Manufacturing", "Deck", "Tools"]
+  const [active, setActive] = useState('Dashboard')
+  const options = ["Home", "Dashboard", "Analytics"]
 
   return (
     <div className="w-full py-1 px-5 flex flex-row justify-between items-center relative">
@@ -20,6 +20,22 @@ export default function Header() {
       />
 
       <SignedIn>
+        <div className="flex gap-6">
+          {options.map(option => (
+            <Link
+              href={`/${option.toLowerCase()}`}
+              key={option}
+              onClick={() => setActive(option)}
+              className={`${
+                active === option
+                  ? "font-semibold"
+                  : "font-normal"
+              } text-[var(--color-base)] text-sm hover:text-[var(--color-base)] hover:scale-105 hover:duration-200 hover:cursor-pointer`}
+            >
+              {option}
+            </Link>
+            ))}
+        </div>
         <div className="px-3"><UserButton /></div>  
       </SignedIn>
     </div>
