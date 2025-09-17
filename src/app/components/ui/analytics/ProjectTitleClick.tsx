144f5e4b6df1e8fd6ffbc5b088d9375cc14c7150
rrhmn <raissarahmani18@gmail.com>
2025-09-17 13:58:45 +0700

    add: analytics feature

---
+++ src/app/components/ui/analytics/ProjectTitleClick.tsx
@@ -0,0 +1,97 @@
+"use client"
+
+import { useAuth } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+interface RankingItem {
+  value: string;
+  count: number;
+}
+
+export default function UserVisit() {
+  const [rankingByType, setRankingByType] = useState<RankingItem[]>([]);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const {getToken} = useAuth()
+
+  useEffect(() => { 
+    const fetchData = async () => {
+      try {
+        const token = await getToken({ template: "default" });
+        if (!token) throw new Error("No token available");
+        console.log(token)
+
+        const res = await fetch(`${apiUrl}/analytics/view-project/title`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
+        })
+
+        if (!res.ok) throw new Error("Failed to fetch data");
+
+        const data = await res.json() as {
+          code: string;
+          status: number;
+          message: string;
+          data: RankingItem[];
+        };
+        setRankingByType(data.data);
+      } catch (error) {
+        console.error("Analytics fetch error:", error);
+      }
+    };
+
+  void fetchData();
+  }, [apiUrl, getToken]);
+
+  return (
+    <div className="p-5">
+      <div className="w-full">
+        <p className="font-semibold text-sm text-[var(--color-grey)] text-center">Project Visit By Title</p>
+        <ResponsiveContainer width="100%" height={200}>
+          <BarChart data={rankingByType}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis 
+              dataKey="value" 
+              height={20}
+              interval={0} 
+              textAnchor="middle"
+              tick={{ fill: "var(--color-base)", fontSize: 10 }}
+              tickFormatter={(val: string) => {
+                return val.length > 5 ? val.slice(0, 5) + "…" : val;
+              }}
+            />
+            <YAxis 
+              width={40}
+              tick={{ fill: "var(--color-base)", fontSize: 12 }}
+            />
+            <Tooltip 
+              contentStyle={{
+                backgroundColor: "var(--color-purple-light)",
+                borderRadius: "8px",
+                border: "1px solid var(--color-grey-light)",
+                padding: "5px",
+                fontSize: "12px",
+                color: "var(--color-grey)",
+              }}
+            />
+            <Bar 
+              dataKey="count" 
+              fill="var(--color-purple)" 
+              radius={[10, 10, 0, 0]}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  )
+}
